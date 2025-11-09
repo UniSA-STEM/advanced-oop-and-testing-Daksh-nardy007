0e6d3083a263404872434fea8c0f919671f4b399
narang001 <dakshnarang5@gmail.com>
2025-11-09 23:17:58 +1030

    Author, ID and username

--- staff.py
+++ staff.py
@@ -1,8 +1,8 @@
 '''
 File: filename.py
 Description: A brief description of this Python module.
-Author: Billy Bizilis
-ID: 110100110
-Username: bizvy001
+Author: Daksh Narang
+ID: 110402195
+Username: nardy007
 This is my own work as defined by the University's Academic Integrity Policy.
 ''
