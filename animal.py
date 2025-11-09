382bfa843211354295e5ce1e601aa4569a246162
narang001 <dakshnarang5@gmail.com>
2025-11-09 23:54:43 +1030

    Add Initialise method in the animal class with required attributes

--- animal.py
+++ animal.py
@@ -5,4 +5,14 @@ Author: Billy Bizilis
 ID: 110100110
 Username: bizvy001
 This is my own work as defined by the University's Academic Integrity Policy.
-''
+'''
+class Animal:
+    def __init__(self, name, species, age, diet, category, environment):
+        self.__name= name
+        self.__species = species
+        self.__age = age
+        self.__diet = diet
+        self.__category = category
+        self.__environment = environment
+        self.__health_record = None
+
