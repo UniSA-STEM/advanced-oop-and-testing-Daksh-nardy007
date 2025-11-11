a01424eace11fb5940fa2bbb8310748b7c81f30a
narang001 <dakshnarang5@gmail.com>
2025-11-11 22:09:07 +1030

    Created enclosure class with add and remove method

--- enclosure.py
+++ enclosure.py
@@ -5,4 +5,22 @@ Author: Billy Bizilis
 ID: 110100110
 Username: bizvy001
 This is my own work as defined by the University's Academic Integrity Policy.
-''
+'''
+class Enclosure:
+    def __init__(self,name, environment, capacity):
+        self.name = name
+        self.environment = environment
+        self.capacity = capacity
+        self.cleanliness = 100
+        self.animals =[]
+
+    def addAnimal(self, animal):
+        self.animals.append(animal)
+    def removeAnimal(self,animal):
+        if animal in self.animals:
+            self.animals.remove(animal)
+
+    def cleanEnclosure(self):
+        self.cleanliness = 100
+        return self.name + "cleaned"
+
